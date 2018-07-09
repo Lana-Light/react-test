7cf1f902cbb4096d2ba97b666924f46db3652e7a
Svetlana <39729453+Lana-Light@users.noreply.github.com>
2018-07-09 16:18:24 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import "styles.css";
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox!</h1>
       <h2>Start editing to see some magic happen!</h2>
-    <p>Test with styles.</p>
+    <p>Test css.</p>
     </div>
   );
 }
